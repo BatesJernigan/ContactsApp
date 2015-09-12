7db0ba8d21567c29c8c31118cd6f74c23d2c8184
Tempestt <tswinso1@uncc.edu>
2015-09-12 03:12:40 -0400

    started the basic layout of page

--- app/src/main/java/com/example/contactsapp/EditContactActivity.java
+++ app/src/main/java/com/example/contactsapp/EditContactActivity.java
@@ -1,37 +1,35 @@
-package com.example.contactsapp;
+    package com.example.contactsapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+    import android.content.Intent;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.Toast;
 
-public class EditContactActivity extends AppCompatActivity {
+    public class EditContactActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_contact);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_edit_contact);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_contact, menu);
-        return true;
-    }
+            if(getIntent().getExtras() != null) {
+                String name = getIntent().getExtras().getString(MainActivity.NAME_KEY);
+                Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+            } else {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            findViewById(R.id.edit_contact_activity).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
 }
