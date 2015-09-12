13521de304ad71f52c280ed8b7acf2aaf2b61779
batesjernigan <bates1012@aol.com>
2015-09-12 10:49:50 -0400

    Added layout for create view. Should be able to reuse same layout for edit contact. Added logic to go to gallery but not to set the image correctly.

--- app/src/main/java/com/example/contactsapp/CreateContactActivity.java
+++ app/src/main/java/com/example/contactsapp/CreateContactActivity.java
@@ -1,8 +1,12 @@
 package com.example.contactsapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,24 +14,86 @@ import android.widget.Toast;
 
 public class CreateContactActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_create_contact);
+//
+//        if(getIntent().getExtras() != null) {
+//            String name = getIntent().getExtras().getString(MainActivity.NAME_KEY);
+//            Contact contact = getIntent().getExtras().getParcelable(MainActivity.CONTACT_KEY);
+////            Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+//            Toast.makeText(this, contact.toString(), Toast.LENGTH_LONG).show();
+//        } else {
+//
+//        }
+//
+//    }
+
+
+    // this is the action code we use in our intent,
+    // this way we know we're looking at the response from our own action
+    private static final int SELECT_PICTURE = 1;
+
+    private String selectedImagePath;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact);
 
-        if(getIntent().getExtras() != null) {
-            String name = getIntent().getExtras().getString(MainActivity.NAME_KEY);
-            Toast.makeText(this, name, Toast.LENGTH_LONG).show();
-        } else {
-
-        }
-
-        findViewById(R.id.create_contact_activity).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.save_contact_activity).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        findViewById(R.id.gallery_button)
+            .setOnClickListener(new View.OnClickListener() {
+                public void onClick(View arg0) {
+                    // in onCreate or any event where your want the user to
+                    // select a file
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+                }
+            });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("demo", " in on activity result");
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                Log.d("demo", "selectedImagePath " + selectedImagePath);
+            }
+        }
+    }
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
     }
 
 }
